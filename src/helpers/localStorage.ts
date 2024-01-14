f20ed5a6d3e419ac85acc0d6643de15011ed6ff2
Sergey Shabanov <shsv382@gmail.com>
2024-01-14 20:10:37 +0300

    Добавлены исправления в LocalStorage

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -3,38 +3,70 @@ type TempItem = {
     value: any
 }
 
+class LocalStorageError extends Error {
+    constructor(message: string) {
+      super(message);
+      this.name = this.constructor.name;
+      Object.setPrototypeOf(this, new.target.prototype);
+    }
+}
+
 export class LocalStorage {
     static setTemporaryItem(key: string, value: any, minutesToExpire: number) {
-        let exp = new Date();
-        exp.setTime(exp.getTime() + (minutesToExpire * 60 * 1000));
-        localStorage.setItem(
-            key, 
-            JSON.stringify({ exp, value })
-        )
+        try {
+            let exp = new Date();
+            exp.setTime(exp.getTime() + (minutesToExpire * 60 * 1000));
+            localStorage.setItem(
+                key, 
+                JSON.stringify({ exp, value })
+            )
+            return { exp, value };
+        } catch(error) {
+            throw new LocalStorageError("Unable to set temporary item to localStorage")
+        }
     }
 
     static getTemporaryItem(key: string) {
-        let item: string | object | null = localStorage.getItem(key);
-        if (!item) return null;
-        let itemObj: TempItem = JSON.parse(item);
-        if (new Date() > new Date(itemObj.exp)) {
-            localStorage.removeItem(key);
-            return null;
+        try {
+            let item: string | object | null = localStorage.getItem(key);
+            if (!item) return null;
+            let itemObj: TempItem = JSON.parse(item);
+            if (new Date() > new Date(itemObj.exp)) {
+                return null;
+            }
+            return itemObj.value
+        } catch(error) {
+            throw new LocalStorageError("Unable to get temporary item from localStorage")
         }
-        return itemObj.value
     }
 
     static setItem(key: string, value: any) {
-        localStorage.setItem(
-            key, 
-            JSON.stringify({ value })
-        )
+        try {
+            localStorage.setItem(
+                key, 
+                JSON.stringify({ value })
+            )
+        } catch(error) {
+            throw new LocalStorageError("Unable to set item to localStorage")
+        }
     }
 
     static getItem(key: string) {
-        let item: string | object | null = localStorage.getItem(key);
-        if (!item) return null;
-        let itemObj: TempItem = JSON.parse(item);
-        return itemObj.value
+        try {
+            let item: string | object | null = localStorage.getItem(key);
+            if (!item) return null;
+            let itemObj: TempItem = JSON.parse(item);
+            return itemObj.value
+        } catch(error) {
+            throw new LocalStorageError("Unable to get item from localStorage")
+        }
+    }
+
+    static removeItem(key: string) {
+        try {
+            localStorage.removeItem(key);
+        } catch(error) {
+            throw new LocalStorageError("Unable to remove item from localStorage")
+        }
     }
 }
